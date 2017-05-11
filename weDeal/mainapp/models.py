4661051cef8f832190972d8479f8d0632b15190f
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2017-05-11 03:42:43 -0400

    massive changes

--- weDeal/mainapp/models.py
+++ weDeal/mainapp/models.py
@@ -11,10 +11,28 @@ class Deal(models.Model):
     number_of_items = models.IntegerField()
     ending_date = models.DateField()
     image = models.ImageField(upload_to='images/')
+    sold_items = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
 
     @property
     def unit_price(self):
         return self.total_price / self.number_of_items
+
+    @property
+    def available_items(self):
+        return self.number_of_items - self.sold_items
+
+    @property
+    def percentage_taken(self):
+        return 100*self.sold_items / self.number_of_items
+
+    @property
+    def checked_stars_str(self):
+        return '*' * self.rating
+
+    @property
+    def unchecked_stars_str(self):
+        return '*' * (5 - self.rating)
 
     def __str__(self):
         return self.name
